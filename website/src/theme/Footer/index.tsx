81562e236531e265586a4c3e87b891027ebfd035
Naomi Aro <naomiaro@gmail.com>
2025-12-05 16:16:52 -0800

    coffee

--- website/src/theme/Footer/index.tsx
+++ website/src/theme/Footer/index.tsx
@@ -6,11 +6,26 @@ import Sponsors from '@site/src/components/Sponsors';
 
 type Props = WrapperProps<typeof FooterType>;
 
+function BuyMeACoffee() {
+  return (
+    <div style={{ textAlign: 'center', padding: '0.5rem 0' }}>
+      <a href="https://www.buymeacoffee.com/naomiaro" target="_blank" rel="noopener noreferrer">
+        <img
+          src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png"
+          alt="Buy Me A Coffee"
+          style={{ height: '40px', width: '145px' }}
+        />
+      </a>
+    </div>
+  );
+}
+
 export default function FooterWrapper(props: Props): React.ReactNode {
   return (
     <>
       <Sponsors />
       <Footer {...props} />
+      <BuyMeACoffee />
     </>
   );
 }
